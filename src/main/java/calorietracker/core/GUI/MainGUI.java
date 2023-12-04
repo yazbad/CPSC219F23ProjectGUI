9f08991d4c542422e777524d99923ae16a80d38f
yazeed.badr1 <yazeed.badr1@ucalgary.ca>
2023-12-04 04:43:25 -0700

    Fixed issue where MainGUI wouldnt load

--- src/main/java/calorietracker/core/GUI/MainGUI.java
+++ src/main/java/calorietracker/core/GUI/MainGUI.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class MainGUI extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainGUI.class.getResource("calorietracker.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainGUI.class.getResource("/calorietracker/core/calorietracker.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Calorie Tracking System");
         stage.setScene(scene);
